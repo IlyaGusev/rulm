716255cb1b2eee32914d3c886b93bd7a6c6c4381
Ilya Gusev <phoenixilya@gmail.com>
2018-09-24 19:38:03 +0300

    Main fixed

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 from rulm.vocabulary import Vocabulary
 from rulm.ngrams import NGramLanguageModel
-from rulm.filter import AlphabetOrderFilter
+from rulm.transform import AlphabetOrderTransform
 from nltk import WordPunctTokenizer
 
-file_name = "rdt.example.txt"
-vocabulary = Vocabulary()
-#i = 0
-#with open(file_name, "r") as r:
-#    for line in r:
-#        i += 1
-#        for word in line.strip().split():
-#            vocabulary.add_word(word)
-#        if i % 100000 == 0:
-#            print("Vocab:", i)
-#vocabulary.sort()
-vocabulary.load("vocab_rdt_example.txt")
+file_name = "rulm/data/rdt.example.txt"
+vocab_file_name = "rulm/data/vocab.rdt.example.txt"
 
-sentences = []
-with open(file_name, "r") as r:
-    for line in r:
-        words = line.strip().split()
-        sentences.append(words)
-f = AlphabetOrderFilter(vocabulary)
-model = NGramLanguageModel(4, vocabulary, f)
-model.train(sentences)
-print(model.sample_decoding(["В"], k=5))
+vocabulary = Vocabulary()
+vocabulary.load(vocab_file_name)
+
+f = AlphabetOrderTransform(vocabulary)
+model = NGramLanguageModel(3, vocabulary, (f, ), (1.0, 0.01, 0.0001))
+model.train_file(file_name)
+
+print(model.beam_decoding([], beam_width=20))
 #model.measure_perplexity(sentences)
