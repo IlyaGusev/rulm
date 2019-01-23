83933e2ea0c78f7c9a0d39ea827ef82d63048f3c
Ilya Gusev <phoenixilya@gmail.com>
2019-01-24 01:09:56 +0300

    Various dropouts

--- rulm/models/neural_net/encoder_only.py
+++ rulm/models/neural_net/encoder_only.py
@@ -2,13 +2,17 @@ from typing import Dict
 
 import numpy as np
 import torch
-from torch.nn.functional import linear, log_softmax
+from torch.nn.functional import linear, log_softmax, embedding
 from torch.nn import Dropout, LogSoftmax, NLLLoss
+from allennlp.common import Params
 from allennlp.models.model import Model
 from allennlp.data.vocabulary import Vocabulary, DEFAULT_PADDING_TOKEN
-from allennlp.modules import TextFieldEmbedder
-from allennlp.modules import Seq2SeqEncoder
+from allennlp.modules import TextFieldEmbedder, TimeDistributed, Seq2SeqEncoder
 from allennlp.modules.sampled_softmax_loss import SampledSoftmaxLoss
+from allennlp.modules.input_variational_dropout import InputVariationalDropout
+from allennlp.modules.token_embedders import Embedding, TokenEmbedder
+from allennlp.modules.token_embedders.embedding import _read_pretrained_embeddings_file
+from allennlp.nn.util import combine_initial_dims, uncombine_initial_dims
 
 
 class SoftmaxLoss(torch.nn.Module):
@@ -35,6 +39,91 @@ class SoftmaxLoss(torch.nn.Module):
         return criterion(logits, targets.long())
 
 
+@TokenEmbedder.register("embedding_with_dropout")
+class EmbeddingWithDropout(Embedding):
+    def __init__(self,
+                 num_embeddings: int,
+                 embedding_dim: int,
+                 dropout: float = None,
+                 projection_dim: int = None,
+                 weight: torch.FloatTensor = None,
+                 padding_index: int = None,
+                 trainable: bool = True,
+                 max_norm: float = None,
+                 norm_type: float = 2.,
+                 scale_grad_by_freq: bool = False,
+                 sparse: bool = False) -> None:
+        Embedding.__init__(self,
+                           num_embeddings=num_embeddings,
+                           embedding_dim=embedding_dim,
+                           projection_dim=projection_dim,
+                           weight=weight,
+                           padding_index=padding_index,
+                           trainable=trainable,
+                           max_norm=max_norm,
+                           norm_type=norm_type,
+                           scale_grad_by_freq=scale_grad_by_freq,
+                           sparse=sparse)
+        self.dropout = dropout
+
+    def forward(self, inputs):
+        original_size = inputs.size()
+        inputs = combine_initial_dims(inputs)
+
+        if self.dropout and self.training:
+            mask = self.weight.data.new().resize_((self.weight.size(0), 1)).bernoulli_(1 - self.dropout)\
+                       .expand_as(self.weight) / (1 - self.dropout)
+            masked_embed_weight = mask * self.weight
+        else:
+            masked_embed_weight = self.weight
+
+        embedded = embedding(inputs, masked_embed_weight,
+                             max_norm=self.max_norm,
+                             norm_type=self.norm_type,
+                             scale_grad_by_freq=self.scale_grad_by_freq,
+                             sparse=self.sparse)
+
+        embedded = uncombine_initial_dims(embedded, original_size)
+
+        if self._projection:
+            projection = self._projection
+            for _ in range(embedded.dim() - 2):
+                projection = TimeDistributed(projection)
+            embedded = projection(embedded)
+        return embedded
+
+    @classmethod
+    def from_params(cls, vocab: Vocabulary, params: Params) -> 'Embedding':
+        num_embeddings = params.pop_int('num_embeddings', None)
+        vocab_namespace = params.pop("vocab_namespace", "tokens")
+        if num_embeddings is None:
+            num_embeddings = vocab.get_vocab_size(vocab_namespace)
+        embedding_dim = params.pop_int('embedding_dim')
+        pretrained_file = params.pop("pretrained_file", None)
+        projection_dim = params.pop_int("projection_dim", None)
+        trainable = params.pop_bool("trainable", True)
+        padding_index = params.pop_int('padding_index', None)
+        max_norm = params.pop_float('max_norm', None)
+        norm_type = params.pop_float('norm_type', 2.)
+        scale_grad_by_freq = params.pop_bool('scale_grad_by_freq', False)
+        sparse = params.pop_bool('sparse', False)
+        dropout = params.pop_float('dropout', None)
+        params.assert_empty(cls.__name__)
+        weight = _read_pretrained_embeddings_file(pretrained_file, embedding_dim,
+                                                  vocab, vocab_namespace) if pretrained_file else None
+        return cls(num_embeddings=num_embeddings,
+                   embedding_dim=embedding_dim,
+                   projection_dim=projection_dim,
+                   weight=weight,
+                   padding_index=padding_index,
+                   trainable=trainable,
+                   max_norm=max_norm,
+                   norm_type=norm_type,
+                   scale_grad_by_freq=scale_grad_by_freq,
+                   sparse=sparse,
+                   dropout=dropout)
+
+
 @Model.register("encoder_only")
 class EncoderOnlyLanguageModel(Model):
     def __init__(self,
@@ -43,13 +132,18 @@ class EncoderOnlyLanguageModel(Model):
                  contextualizer: Seq2SeqEncoder,
                  dropout: float = None,
                  tie_embeddings: bool = True,
-                 num_samples: int = None):
+                 num_samples: int = None,
+                 use_variational_dropout: bool = False):
         super().__init__(vocab)
 
         self._embedder = embedder
         self._contextualizer = contextualizer
         self._context_dim = contextualizer.get_output_dim()
-        self._dropout = Dropout(dropout) if dropout else lambda x: x
+
+        if use_variational_dropout:
+            self._dropout = InputVariationalDropout(dropout) if dropout else lambda x: x
+        else:
+            self._dropout = Dropout(dropout) if dropout else lambda x: x
 
         vocab_size = self.vocab.get_vocab_size()
         padding_index = self.vocab.get_token_index(DEFAULT_PADDING_TOKEN)
@@ -60,10 +154,10 @@ class EncoderOnlyLanguageModel(Model):
 
         self._tie_embeddings = tie_embeddings
         if self._tie_embeddings:
-            assert "token_embedder_tokens" in dict(self._embedder.named_children())
-            source_token_embedder = dict(self._embedder.named_children())["token_embedder_tokens"]
-            assert self._softmax_loss.softmax_w.size() == source_token_embedder.weight.size()
-            self._softmax_loss.softmax_w = source_token_embedder.weight
+            embedder_children = dict(self._embedder.named_children())
+            word_embedder = embedder_children["token_embedder_tokens"]
+            assert self._softmax_loss.softmax_w.size() == word_embedder.weight.size()
+            self._softmax_loss.softmax_w = word_embedder.weight
 
     def forward(self,
                 source_tokens: Dict[str, torch.Tensor],
@@ -75,6 +169,7 @@ class EncoderOnlyLanguageModel(Model):
 
         # Shape: (batch_size, max_length, embedding_size)
         embeddings = self._embedder(source_tokens)
+        embeddings = self._dropout(embeddings)
 
         # Shape: (batch_size, max_length, context_dim)
         contextual_embeddings = self._contextualizer(embeddings, mask)
