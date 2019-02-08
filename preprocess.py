b265ffb4b52071b25f5e3c499dd45299b9e259e6
Ilya Gusev <phoenixilya@gmail.com>
2019-02-08 13:53:40 +0300

    Scripts fixes

--- preprocess.py
+++ preprocess.py
@@ -9,20 +9,15 @@ from rulm.language_model import LanguageModel
 
 
 def preprocess(train_path, vocabulary_path, config_path):
-    vocabulary_path = vocabulary_path
-    config_path = config_path
     params = Params.from_file(config_path)
 
-    vocabulary_params = params.pop("vocabulary", default=Params({}))
-    reader_params = params.duplicate().pop("reader", default=Params({}))
+    reader_params = params.pop("reader", default=Params({}))
     reader = DatasetReader.from_params(reader_params)
     dataset = reader.read(train_path)
 
-    if os.path.isdir(vocabulary_path):
-        vocabulary = Vocabulary.from_files(vocabulary_path)
-    else:
-        vocabulary = Vocabulary.from_params(vocabulary_params, instances=dataset)
-        vocabulary.save_to_files(vocabulary_path)
+    vocabulary_params = params.pop("vocabulary", default=Params({}))
+    vocabulary = Vocabulary.from_params(vocabulary_params, instances=dataset)
+    vocabulary.save_to_files(vocabulary_path)
 
 
 if __name__ == "__main__":
@@ -32,4 +27,4 @@ if __name__ == "__main__":
     parser.add_argument('--vocabulary-path', required=True)
     parser.add_argument('--config-path', required=True)
     args = parser.parse_args()
-    preprocess(args.train_path, args.vocabulary_path, args.config_path)
+    preprocess(**vars(args))
--- train.py
+++ train.py
@@ -8,22 +8,13 @@ from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from rulm.language_model import LanguageModel
 
 
-def train(model_path, train_path, val_path, vocabulary_path=None, config_path=None):
+def train(model_path, train_path, val_path=None, vocabulary_path=None, config_path=None):
     vocabulary_path = vocabulary_path or os.path.join(model_path, "vocabulary")
+    assert os.path.isdir(vocabulary_path), "Can't find vocab, run preprocess.py first"
+    vocabulary = Vocabulary.from_files(vocabulary_path)
+
     config_path = config_path or os.path.join(model_path, "config.json")
     params = Params.from_file(config_path)
-
-    vocabulary_params = params.pop("vocabulary", default=Params({}))
-    reader_params = params.duplicate().pop("reader", default=Params({}))
-    reader = DatasetReader.from_params(reader_params)
-    dataset = reader.read(train_path)
-
-    if os.path.isdir(vocabulary_path):
-        vocabulary = Vocabulary.from_files(vocabulary_path)
-    else:
-        vocabulary = Vocabulary.from_params(vocabulary_params, instances=dataset)
-        vocabulary.save_to_files(vocabulary_path)
-
     train_params = params.pop("train", Params({}))
     model = LanguageModel.from_params(params, vocab=vocabulary)
     model.train(train_path, train_params, serialization_dir=model_path, valid_file_name=val_path)
@@ -37,10 +28,4 @@ if __name__ == "__main__":
     parser.add_argument('--vocabulary-path')
     parser.add_argument('--config-path')
     args = parser.parse_args()
-    train(
-        args.model_path,
-        args.train_path,
-        args.val_path,
-        vocabulary_path=args.vocabulary_path,
-        config_path=args.config_path
-    )
+    train(**vars(args))
