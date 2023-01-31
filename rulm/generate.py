d0cc2e98a2f03d63f2077961ce5cb434f23122a2
Ilya Gusev <phoenixilya@gmail.com>
2023-01-31 19:44:19 +0000

    New configs

--- rulm/generate.py
+++ rulm/generate.py
@@ -8,10 +8,16 @@ model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.padding_side = 'left'
 
-inp = "И грянул"
-tokens = tokenizer(inp, return_tensors="pt")["input_ids"][:, :-1]
+inp = ""
+tokens = tokenizer(inp, return_tensors="pt")
+input_ids = tokens["input_ids"][:, :-1]
+attention_mask = tokens["attention_mask"][:, :-1]
+position_ids = attention_mask.cumsum(-1)
+token_type_ids = tokens["token_type_ids"][:, :-1]
 print(tokens)
 output_ids = model.generate(
-    input_ids=tokens, max_length=64, num_beams=1, do_sample=True, top_p=0.95
+    input_ids=input_ids,
+    attention_mask=attention_mask, token_type_ids=token_type_ids,
+    max_length=64, num_beams=1, do_sample=True, top_p=0.95
 )[0]
 print(tokenizer.decode(output_ids))
