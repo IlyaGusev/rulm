b8da37c942ed2d1ea3759bc82087912803d51d03
Ilya Gusev <phoenixilya@gmail.com>
2023-04-09 20:28:19 +0000

    Better interact

---
+++ self_instruct/scripts/interact.py
@@ -0,0 +1,81 @@
+import sys
+import fire
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+from peft import PeftModel, PeftConfig
+import torch
+
+from utils import Conversation
+
+
+def generate(model, tokenizer, prompt, generation_config, eos_token_id: int = None):
+    data = tokenizer(prompt, return_tensors="pt")
+    data = {k: v.to(model.device) for k, v in data.items()}
+    if eos_token_id is not None:
+        generation_config.eos_token_id = eos_token_id
+    output_ids = model.generate(
+        **data,
+        generation_config=generation_config
+    )[0]
+    output_ids = output_ids[len(data["input_ids"][0]):]
+    output = tokenizer.decode(output_ids)
+    return output
+
+
+def interact(model_name, template_path):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    generation_config = GenerationConfig.from_pretrained(model_name)
+
+    if device == "cuda":
+        config = PeftConfig.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(
+            config.base_model_name_or_path,
+            torch_dtype=torch.float16,
+            load_in_8bit=True,
+            device_map="auto"
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            model_name,
+            torch_dtype=torch.float16
+        )
+    elif device == "cpu":
+        config = PeftConfig.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(
+            config.base_model_name_or_path,
+            device_map={"": device},
+            low_cpu_mem_usage=True
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            model_name,
+            device_map={"": device}
+        )
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    conversation = Conversation.from_template(template_path)
+    while True:
+        user_message = input("User: ")
+        if user_message.strip() == "/reset":
+            conversation = Conversation.from_template(template_path)
+            print("History reset completed!")
+            continue
+        conversation.add_user_message(user_message)
+        prompt = conversation.get_prompt()
+        output = generate(
+            model=model,
+            tokenizer=tokenizer,
+            prompt=prompt,
+            generation_config=generation_config,
+            eos_token_id=conversation.get_end_token_id()
+        )
+        output = output.replace("<end", "").replace("bot", "").strip()
+        conversation.add_bot_message(output)
+        print("Saiga:", output)
+
+
+if __name__ == "__main__":
+    fire.Fire(interact)
--- self_instruct/scripts/utils.py
+++ self_instruct/scripts/utils.py
@@ -154,3 +154,75 @@ def fix_model(model, tokenizer, use_resize=True):
         model.resize_token_embeddings(len(tokenizer))
 
     return model
+
+
+DEFAULT_MESSAGE_TEMPLATE = "<start>{role}\n{content} <end>\n",
+DEFAULT_SYSTEM_PROMPT = "Ты — Сайга, русскоязычный автоматический ассистент. Ты разговариваешь с людьми и помогаешь им."
+DEFAULT_START_TOKEN_ID = 2962
+DEFAULT_END_TOKEN_ID = 355
+DEFAULT_BOT_TOKEN_ID = 7451
+
+
+class Conversation:
+    def __init__(
+        self,
+        message_template=DEFAULT_MESSAGE_TEMPLATE,
+        system_prompt=DEFAULT_SYSTEM_PROMPT,
+        role_mapping=None,
+        start_token_id=DEFAULT_START_TOKEN_ID,
+        end_token_id=DEFAULT_END_TOKEN_ID,
+        bot_token_id=DEFAULT_BOT_TOKEN_ID
+    ):
+        self.message_template = message_template
+        self.role_mapping = role_mapping or {}
+        self.start_token_id = start_token_id
+        self.end_token_id = end_token_id
+        self.bot_token_id = bot_token_id
+        self.messages = [{
+            "role": "system",
+            "content": system_prompt
+        }]
+
+    def get_end_token_id(self):
+        return self.end_token_id
+
+    def get_start_token_id(self):
+        return self.start_token_id
+
+    def get_bot_token_id(self):
+        return self.bot_token_id
+
+    def add_user_message(self, message):
+        self.messages.append({
+            "role": "user",
+            "content": message
+        })
+
+    def add_bot_message(self, message):
+        self.messages.append({
+            "role": "bot",
+            "content": message
+        })
+
+    def get_prompt(self):
+        final_text = ""
+        for message in self.messages:
+            message_text = self.message_template.format(**message)
+            final_text += message_text
+        final_text += "<start>"
+        return final_text
+
+    @classmethod
+    def from_template(cls, file_name):
+        with open(file_name) as r:
+            template = json.load(r)
+        return Conversation(
+            **template
+        )
+
+    def expand(self, messages):
+        for message in messages:
+            self.messages.append({
+                "role": self.role_mapping.get(message["role"], message["role"]),
+                "content": message["content"]
+            })
