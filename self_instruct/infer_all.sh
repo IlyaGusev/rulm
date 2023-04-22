1a70ea2ee35b688890e3ee196f5c41b583107b7c
IlyaGusev <phoenixilya@gmail.com>
2023-04-22 19:35:21 +0000

    New answers

--- self_instruct/infer_all.sh
+++ self_instruct/infer_all.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-# Saiga 30B
-#python3 -m src.infer_saiga \
-#    --model-name IlyaGusev/saiga_30b_lora \
-#    --template-path internal_prompts/saiga_v2.json \
-#    --input-path data/vicuna_question_ru.jsonl \
-#    --output-path data/vicuna_saiga30b_answers.jsonl \
-#    --batch-size 8
+echo "========= Infering Saiga 30B ========="
+python3 -m src.infer_saiga \
+    --model-name IlyaGusev/saiga_30b_lora \
+    --template-path internal_prompts/saiga_v2.json \
+    --input-path data/vicuna_question_ru.jsonl \
+    --output-path data/vicuna_saiga30b_answers.jsonl \
+    --batch-size 8
 
-#python3 -m src.infer_saiga \
-#    --model-name models/saiga_30b_lora \
-#    --template-path internal_prompts/saiga_v2.json \
-#    --input-path data/user_oriented_ru.jsonl \
-#    --output-path data/user_saiga30b_answers.jsonl \
-#    --batch-size 4
+python3 -m src.infer_saiga \
+    --model-name models/saiga_30b_lora \
+    --template-path internal_prompts/saiga_v2.json \
+    --input-path data/user_oriented_ru.jsonl \
+    --output-path data/user_saiga30b_answers.jsonl \
+    --batch-size 4
 
-# Saiga 7B
+echo "========= Infering Saiga 7B ========="
 python3 -m src.infer_saiga \
     --model-name IlyaGusev/saiga_7b_lora \
     --template-path internal_prompts/saiga_v2.json \
@@ -31,7 +31,7 @@ python3 -m src.infer_saiga \
     --output-path data/user_saiga7b_answers.jsonl \
     --batch-size 8
 
-# Saiga 13B
+echo "========= Infering Saiga 13B ========="
 python3 -m src.infer_saiga \
     --model-name IlyaGusev/saiga_13b_lora \
     --template-path internal_prompts/saiga_v2.json \
@@ -46,7 +46,7 @@ python3 -m src.infer_saiga \
     --output-path data/user_saiga13b_answers.jsonl \
     --batch-size 4
 
-# Alpaca 13B
+echo "========= Infering Alpaca 13B ========="
 python3 -m src.infer_alpaca \
     --model-name IlyaGusev/llama_13b_ru_turbo_alpaca_lora \
     --template-path internal_prompts/ru_alpaca.json \
@@ -61,7 +61,7 @@ python3 -m src.infer_alpaca \
     --output-path data/user_rualpaca13b_answers.jsonl \
     --batch-size 1
 
-# Alpaca 7B
+echo "========= Infering Alpaca 7B ========="
 python3 -m src.infer_alpaca \
     --model-name IlyaGusev/llama_7b_ru_turbo_alpaca_lora \
     --template-path internal_prompts/ru_alpaca_old.json \
