8a35d8b85c05d9144ff31fe72399cd5a2b98427a
Ilya Gusev <phoenixilya@gmail.com>
2023-04-22 21:07:07 +0000

    Update merging scripts

--- self_instruct/src/bot/merge_sides.py
+++ self_instruct/src/bot/merge_sides.py
@@ -2,54 +2,61 @@ import csv
 import sys
 import json
 import random
+from collections import defaultdict
+from itertools import combinations
 
 from src.util.io import read_jsonl
 
-input1 = sys.argv[1]
-input2 = sys.argv[2]
-model1_id = sys.argv[3]
-model2_id = sys.argv[4]
-output_path = sys.argv[5]
+output_path = sys.argv[1]
 
-if input1.endswith("csv"):
-    records1 = []
-    with open(input1) as r:
-        reader = csv.reader(r)
-        header = next(reader)
-        for row in reader:
-            record = dict(zip(header, row))
-            record["instruction"] = record.pop("Instruction")
-            record["answer"] = record.pop("Ans RetrivalR").split("<instructionS>")[-1].replace("<instructionE>", "").strip()
-            records1.append(record)
-elif input1.endswith("jsonl"):
-    records1 = read_jsonl(input1)
+files = [
+    {
+        "file_name": "data/vicuna_saiga7b_answers.jsonl",
+        "model_name": "saiga7b",
+    },
+    {
+        "file_name": "data/vicuna_saiga30b_answers.jsonl",
+        "model_name": "saiga30b",
+    },
+    {
+        "file_name": "data/user_saiga7b_answers.jsonl",
+        "model_name": "saiga7b",
+    },
+    {
+        "file_name": "data/user_saiga30b_answers.jsonl",
+        "model_name": "saiga30b",
+    }
+]
+model_count = len({r["model_name"] for r in files})
 
-if input2.endswith("csv"):
-    records2 = []
-    with open(input2) as r:
-        reader = csv.reader(r)
-        header = next(reader)
-        for row in reader:
-            record = dict(zip(header, row))
-            record["instruction"] = record.pop("Instruction")
-            record["answer"] = record.pop("Ans ClassicR").split("<instructionS>")[-1].replace("<instructionE>", "").strip()
-            records2.append(record)
-elif input2.endswith("jsonl"):
-    records2 = read_jsonl(input2)
+def get_key(record):
+    if "input" not in record:
+        return (record["instruction"], )
+    return (record["instruction"], record["input"])
 
+answers = defaultdict(list)
+for r in files:
+    file_name = r["file_name"]
+    model_name = r["model_name"]
+    records = read_jsonl(file_name)
+    for record in records:
+        record["model_name"] = model_name
+        answers[get_key(record)].append(record)
+
+for key, outputs in answers.items():
+    assert len(outputs) == model_count
 
 with open(output_path, "w") as w:
-    for r1, r2 in zip(records1, records2):
-        assert r1["instruction"] == r2["instruction"]
-        a_answer = r1["answer"].replace("assistant", "").strip()
-        b_answer = r2["answer"].replace("assistant", "").strip()
-        record = {k: v for k, v in r1.items() if k in ("instruction", "input")}
-        a_model, b_model = model1_id, model2_id
-        if random.random() < 0.5:
-            a_model, b_model = b_model, a_model
-            a_answer, b_answer = b_answer, a_answer
-        record["a"] = a_answer
-        record["b"] = b_answer
-        record["a_model"] = a_model
-        record["b_model"] = b_model
-        w.write(json.dumps(record, ensure_ascii=False).strip() + "\n")
+    for key, outputs in answers.items():
+        all_answers = [(r["model_name"], r["answer"]) for r in outputs]
+        record = {k: v for k, v in outputs[0].items() if k in ("instruction", "input")}
+        for comb in combinations(all_answers, 2):
+            (a_model, a_answer), (b_model, b_answer) = comb
+            if random.random() < 0.5:
+                a_model, b_model = b_model, a_model
+                a_answer, b_answer = b_answer, a_answer
+            record["a"] = a_answer
+            record["b"] = b_answer
+            record["a_model"] = a_model
+            record["b_model"] = b_model
+            w.write(json.dumps(record, ensure_ascii=False).strip() + "\n")
