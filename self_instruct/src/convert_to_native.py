90a08ecec473ca3601fdf0331f7fd647ee37e251
Ilya Gusev <phoenixilya@gmail.com>
2023-05-26 22:30:21 +0000

    convert_to_native on gpu

--- self_instruct/src/convert_to_native.py
+++ self_instruct/src/convert_to_native.py
@@ -1,13 +1,12 @@
-import sys
-import json
-import os
+import argparse
 
 import torch
 from peft import PeftModel, PeftConfig
 from transformers import LlamaForCausalLM
+from tqdm.auto import tqdm
 
 
-def unpermute(w):
+def unpermute(w, n_heads, dim):
     return (
         w.view(n_heads, 2, dim // n_heads // 2, dim)
         .transpose(1, 2)
@@ -50,54 +49,72 @@ def translate_state_dict_key(k):  # noqa: C901
     else:
         raise NotImplementedError
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_name')
+    parser.add_argument('output_path')
+    parser.add_argument('--device', default='cpu')
+    parser.add_argument('--enable_offloading', action='store_true')
+    args = parser.parse_args()
 
-model_name = sys.argv[1]
-output_path = sys.argv[2]
-assert output_path.endswith(".pt")
+    assert args.output_path.endswith(".pt")
 
-config = PeftConfig.from_pretrained(model_name)
-base_model_path = config.base_model_name_or_path
+    config = PeftConfig.from_pretrained(args.model_name)
+    base_model_path = config.base_model_name_or_path
 
-base_model = LlamaForCausalLM.from_pretrained(
-    base_model_path,
-    load_in_8bit=False,
-    torch_dtype=torch.float16,
-    device_map={'': 'cpu'},
-)
+    base_model = LlamaForCausalLM.from_pretrained(
+        base_model_path,
+        load_in_8bit=False,
+        torch_dtype=torch.float16,
+        device_map={'': args.device},
+    )
 
-lora_model = PeftModel.from_pretrained(
-    base_model,
-    model_name,
-    device_map={'': 'cpu'},
-    torch_dtype=torch.float16,
-)
+    lora_model = PeftModel.from_pretrained(
+        base_model,
+        args.model_name,
+        device_map={'': args.device},
+        torch_dtype=torch.float16,
+    )
 
-lora_model = lora_model.merge_and_unload()
-lora_model.train(False)
+    lora_model = lora_model.merge_and_unload()
+    lora_model.train(False)
 
-if '7b' in base_model_path:
-    n_layers = 32
-    n_heads = 32
-    dim = 4096
-elif '13b' in base_model_path:
-    n_layers = 40
-    n_heads = 40
-    dim = 5120
-elif '30b' in base_model_path:
-    n_layers = 60
-    n_heads = 52
-    dim = 6656
-else:
-    raise NotImplementedError
+    if '7b' in base_model_path:
+        n_layers = 32
+        n_heads = 32
+        dim = 4096
+    elif '13b' in base_model_path:
+        n_layers = 40
+        n_heads = 40
+        dim = 5120
+    elif '30b' in base_model_path:
+        n_layers = 60
+        n_heads = 52
+        dim = 6656
+    else:
+        raise NotImplementedError
 
-lora_model_sd = lora_model.state_dict()
-new_state_dict = {}
-for k, v in lora_model_sd.items():
-    new_k = translate_state_dict_key(k)
-    if new_k is not None:
-        if 'wq' in new_k or 'wk' in new_k:
-            new_state_dict[new_k] = unpermute(v)
-        else:
-            new_state_dict[new_k] = v
+    lora_model_sd = lora_model.state_dict()
+    del lora_model, base_model
+    total = len(lora_model_sd)
+    with tqdm(list(lora_model_sd.keys())) as progress_bar:
+        for i, k in enumerate(progress_bar):
+            new_k = translate_state_dict_key(k)
+            if new_k is None:
+                continue
+            v = lora_model_sd.pop(k)
+            if 'wq' in new_k or 'wk' in new_k:
+                lora_model_sd[new_k] = unpermute(v, n_heads=n_heads, dim=dim)
+            else:
+                lora_model_sd[new_k] = v
 
-torch.save(new_state_dict, f'{output_path}')
+            if args.enable_offloading and i <= total // 2:
+                # offload half of all tensors to RAM
+                lora_model_sd[new_k] = lora_model_sd[new_k].cpu()
+
+    print('Saving state_dict...')
+    torch.save(lora_model_sd, f'{args.output_path}')
+
+
+if __name__ == '__main__':
+    main()
