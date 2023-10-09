4e6c453f3c39e9bb339b590cf12102218205d87f
Ilya Gusev <phoenixilya@gmail.com>
2023-10-09 17:49:48 +0000

    Interact Mistral

---
+++ self_instruct/src/interact_mistral_llamacpp.py
@@ -0,0 +1,76 @@
+import fire
+from llama_cpp import Llama
+
+SYSTEM_PROMPT = "Ты — Сайга, русскоязычный автоматический ассистент. Ты разговариваешь с людьми и помогаешь им."
+SYSTEM_TOKEN = 1587
+USER_TOKEN = 2188
+BOT_TOKEN = 12435
+LINEBREAK_TOKEN = 13
+
+ROLE_TOKENS = {
+    "user": USER_TOKEN,
+    "bot": BOT_TOKEN,
+    "system": SYSTEM_TOKEN
+}
+
+
+def get_message_tokens(model, role, content):
+    message_tokens = model.tokenize(content.encode("utf-8"))
+    message_tokens.insert(1, ROLE_TOKENS[role])
+    message_tokens.insert(2, LINEBREAK_TOKEN)
+    message_tokens.append(model.token_eos())
+    return message_tokens
+
+
+def get_system_tokens(model):
+    system_message = {
+        "role": "system",
+        "content": SYSTEM_PROMPT
+    }
+    return get_message_tokens(model, **system_message)
+
+
+def interact(
+    model_path,
+    n_ctx=2000,
+    top_k=30,
+    top_p=0.9,
+    temperature=0.2,
+    repeat_penalty=1.1
+):
+    model = Llama(
+        model_path=model_path,
+        n_ctx=n_ctx,
+        n_parts=1,
+    )
+
+    system_tokens = get_system_tokens(model)
+    tokens = system_tokens
+    model.eval(tokens)
+
+    while True:
+        user_message = input("User: ")
+        message_tokens = get_message_tokens(model=model, role="user", content=user_message)
+        role_tokens = [model.token_bos(), BOT_TOKEN, LINEBREAK_TOKEN]
+        tokens += message_tokens + role_tokens
+        print(tokens)
+        full_prompt = model.detokenize(tokens)
+        print(model.tokenize(full_prompt))
+        generator = model.generate(
+            tokens,
+            top_k=top_k,
+            top_p=top_p,
+            temp=temperature,
+            repeat_penalty=repeat_penalty
+        )
+        for token in generator:
+            token_str = model.detokenize([token]).decode("utf-8", errors="ignore")
+            tokens.append(token)
+            if token == model.token_eos():
+                break
+            print(token_str, end="", flush=True)
+        print()
+
+
+if __name__ == "__main__":
+    fire.Fire(interact)
